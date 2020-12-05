f70d3be769dbcbc31f91bd30e43b2970ae11c962
cty288 <cty288@163.com>
2020-12-05 21:03:03 +0800

    2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Main {
-
+	//start
 	public static void main(String[] args) {
 		CourseSystem.Init();
+		//test
 	}
 }
 
